fe0da26225e22d99488bd95b7d1f11ab5ba7e5dd
Zakaria EL MOUMNAOUI <zakaria-statistics>
2025-01-30 02:45:22 +0100

    configure server nginx and enhance angular client display by configuring and installing bootstrap

--- angular-client/src/app/app.routes.ts
+++ angular-client/src/app/app.routes.ts
@@ -5,5 +5,5 @@ import {CustomersComponent} from "./components/customers/customers.component";
 export const routes: Routes = [
   {path: 'accounts', component: AccountsComponent},
   {path: 'customers', component: CustomersComponent},
-  { path: '', redirectTo: 'accounts', pathMatch: 'full' }
+  //{ path: '', redirectTo: 'accounts', pathMatch: 'full' }
 ];
