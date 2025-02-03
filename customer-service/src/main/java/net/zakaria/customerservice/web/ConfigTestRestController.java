5ed4f9a7be42cb214f3d23fe5b4e6b48d0065d34
Zakaria EL MOUMNAOUI <zakaria-statistics>
2025-02-03 01:32:18 +0100

    setting up config service

---
+++ customer-service/src/main/java/net/zakaria/customerservice/web/ConfigTestRestController.java
@@ -0,0 +1,34 @@
+package net.zakaria.customerservice.web;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import net.zakaria.customerservice.config.ConfigParams;
+
+import java.util.Map;
+
+
+@RestController
+@RefreshScope
+public class ConfigTestRestController {
+    @Value("${customer.params.dev1}")
+    private String x;
+    @Value("${customer.params.dev2}")
+    private String y;
+
+    @Autowired
+    private final ConfigParams configParams;
+    
+    @GetMapping("/config1")
+    public Map<String, String> params1(){
+        return Map.of("x",x,"y",y);
+    }
+
+    @GetMapping("/config2")
+    public ConfigParams params2(){
+        return configParams;
+    }
+
+}
